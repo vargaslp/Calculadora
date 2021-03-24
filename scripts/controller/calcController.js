4bb7e05c3e429aa3aac19e12d3c220b54fd4bb41
Lucas Vargas <lucas.p.vargas@gmail.com>
2021-03-24 10:45:23 -0300

    Update calcController.js

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -45,8 +45,38 @@ class CalcController {
 
     }
 
+    getLastOperation() {
+        return this._operation[this._operation.length - 1]
+    }
+
+    isOperator(value) {
+        return (['+', '-', '*', '/'].indexOf(value) > -1)
+
+    }
+
+    setLastOperation(value) {
+        this._operation[this._operation.length - 1] = value
+    }
+
     addOperation(value) {
-        this._operation.push(value)
+
+        if (isNaN(this.getLastOperation())) {
+            if (this.isOperator(value)) {
+                this.setLastOperation(value)
+
+            } else if (isNaN(value)) {
+
+
+            } else {
+                this._operation.push(value)
+
+            }
+
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString()
+            this.setLastOperation(parseInt(newValue))
+        }
         console.log(this._operation)
     }
 
@@ -55,7 +85,9 @@ class CalcController {
     }
 
     addEventListenerAll(element, events, fn) { 
+
         events.split(" ").forEach(event => {
+
             element.addEventListener(event, fn, false)
         })
     }
@@ -71,18 +103,27 @@ class CalcController {
                 this.clearEntry()
                 break
             case 'soma':
+                this._operation.push('+')
                 break
             case 'divisao':
+                this._operation.push('/')
                 break
             case 'multiplicacao':
+                this._operation.push('*')
                 break
             case 'subtracao':
+                this._operation.push('-')
                 break
             case 'porcento':
+                this._operation.push('%')
                 break
             case 'igual':
                 break
-            case 0:
+            case 'ponto':
+                this._operation.push('.')
+                break
+
+            case '0':
             case '1':
             case '2':
             case '3':
@@ -105,7 +146,7 @@ class CalcController {
 
     iniButtonsEvents() {
 
-        let buttons = document.querySelectorAll("#buttons > g, #parts > g")/
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
 
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, "click drag", e => {
